b9cb78d1cbfd96bb7ad40640d94584fcdc54a816
komiwalnut <62009152+komiwalnut@users.noreply.github.com>
2025-08-06 18:04:01 +0800

    Update oauth_utils.py

--- app/utils/oauth_utils.py
+++ app/utils/oauth_utils.py
@@ -10,7 +10,7 @@ from app.models.user import User
 
 GOOGLE_CLIENT_ID = os.getenv("GOOGLE_CLIENT_ID")
 GOOGLE_CLIENT_SECRET = os.getenv("GOOGLE_CLIENT_SECRET")
-GOOGLE_REDIRECT_URI = os.getenv("GOOGLE_REDIRECT_URI", "http://localhost:8000/api/auth/google/callback")
+GOOGLE_REDIRECT_URI = os.getenv("GOOGLE_REDIRECT_URI")
 
 GOOGLE_SCOPES = [
     "https://www.googleapis.com/auth/userinfo.email",
